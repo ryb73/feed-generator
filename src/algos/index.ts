41c3bd5bed3c4d2885748bed943cec4af4234a57
Ryan <rpbiwer@gmail.com>
2025-08-21 03:38:39 +0200

    Update index.ts

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -1,14 +1,14 @@
-import { AppContext } from ‘../config’
+import { AppContext } from '../config'
 import {
-QueryParams,
-OutputSchema as AlgoOutput,
-} from ‘../lexicon/types/app/bsky/feed/getFeedSkeleton’
-import * as localFirst from ‘./local-first’
+  QueryParams,
+  OutputSchema as AlgoOutput,
+} from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import * as localFirst from './local-first'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-[localFirst.shortname]: localFirst.handler,
+  [localFirst.shortname]: localFirst.handler,
 }
 
 export default algos
